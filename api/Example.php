9aab26907a9e057962a3aa420075f2be5afad68d
shellbeat-jpg <shellbeat@luziferase.de>
2026-09-10 22:29:24 +0200

    Remove sensitive API keys from Example.php
    Removed sensitive keys from Example.php for security.

--- api/Example.php
+++ api/Example.php
@@ -1,16 +1,31 @@
-<?php
-set_time_limit(10000);
-
-/* Example usage of the Amazon Product Advertising API */
-include("amazon_api_class.php");
-error_reporting(E_ALL);
-$public_key     = "AKIAIA7YXELZZQTB2T2Q";
-$private_key    = "rSJrJn09YxCQ+n3MZKRyXOw14KL+Mhtnrze1RNZe";
-$region         = "de"; // or "CA" or "DE" etc.
-
-$obj = new AmazonProductAPI($public_key, $private_key, $region);
-$obj->setMedia("display");
-print_r($obj->getBrowseNodes("541686"));
-
-
+<?php
+
+set_time_limit(10000);
+
+
+
+/* Example usage of the Amazon Product Advertising API */
+
+include("amazon_api_class.php");
+
+error_reporting(E_ALL);
+
+$public_key     = "";
+
+$private_key    = "";
+
+$region         = "de"; // or "CA" or "DE" etc.
+
+
+
+$obj = new AmazonProductAPI($public_key, $private_key, $region);
+
+$obj->setMedia("display");
+
+print_r($obj->getBrowseNodes("541686"));
+
+
+
+
+
 ?>
